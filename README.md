19e5e6da03163e7df15cba013a235c84aa2f1287
sragy <sammyragy@gmail.com>
2023-12-24 18:06:51 +0000

    Added class for training. Added README.

---
+++ README.md
@@ -0,0 +1,42 @@
+# Location prediction using density estimation
+
+This is a small toy project to predict the paths of moving objects, e.g. marine vessels. Its main purpose is as a CV-padder, so it is relatively simple, but if it contains any code which you find useful, feel free to use it with reference. This section of the README acts as a write-up and rationale. The [Package Structure](./README.md#Package-Structure) section outlines the package structure and use.
+
+## Theoretical considerations
+### Trajectories or not?
+
+Strictly speaking, this project does not predict trajectories, but it predicts future locations at a set time, i.e. the end-point of the trajectory up to that moment. It could be altered to predict trajectories by outputting a vector of locations, or it could be altered to be conditional on time, so that it can make predictions for arbitrary time points. However, for many purposes this implementation is sufficient; if we are tracking an object using observations at fixed time intervals then information of what is happening *between* those intervals is extraneous.
+
+### Isn't this just object tracking?
+
+No. Object tracking is about consistent identification of over a set of frames. The methods 
+
+Here I assume that a method for object-identification already exits.
+
+However, this is a known problem for which there exist mature methods - Kalman filtering in particular comes to mind. I do not consider Kalman Filtering here, but it should be checked as a bench mark at the very least.
+
+### Stochastic processes need stochastic predictions
+For a stochastic process, even given the exact same input conditions the outcome is not guaranteed. This is sometimes considered to be because of latent unobserved variables, but if you ask a quantum physicist, it is a fundamental property of performing observations.
+
+In cases where certainty is important, it 
+
+## The simulator
+We need a simulator to generate fake data. The simulator is not a critical part of the application - we just need some data from somewhere. I've written a simulator with a toy model of a moving sea vessel. See the below image for 100 simulated trajectories from this simulator with the default settings.
+
+## Density estimators
+Given a set of observations are drawn from some underlying probability distribution
+
+Normalising flows allow us to learn, evaluate, and sample from probability densities. In applications
+where certainty important, but not achievable, then being able to quantify certainty is very valuable.
+
+### Conditioning
+It is not difficult to extend the model to condition upon informative data such as the ship bearing or velocity. However, given that I initialise all ships under the same conditions in the simulator I've written, this is not productive for this project in its current state. Some examples of interesting real-world conditioners:
+
+1. GPS location
+2. Depth-gradient
+3. Bearing of nearest port
+4. Vessel id
+5. Weather
+
+
+# Package Structure
---
+++ training.py
@@ -0,0 +1,210 @@
+from os.path import exists
+
+import torch
+from torch import Tensor
+from torch.nn import CrossEntropyLoss, Module
+from torch.optim import Optimizer, Adam
+from torch.optim.lr_scheduler import _LRScheduler, ReduceLROnPlateau
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from typing import Tuple, Callable
+
+from nflows.flows import Flow
+from nflows.distributions import StandardNormal
+from nflows.transforms import MaskedPiecewiseRationalQuadraticAutoregressiveTransform
+
+from simulator import Simulator
+
+
+def get_default_flow():
+    """Function to get a normalising flow object. Uses a simple default architecture.
+
+    Returns:
+        Flow: A neural spline flow
+    """
+    # We have 2-d vectors on a plane, so shape = features = 2.
+    base_dist = StandardNormal(shape=[2])
+    transform = MaskedPiecewiseRationalQuadraticAutoregressiveTransform(
+        features = 2,
+        hidden_features=20,
+        num_bins=10,
+        tails='linear',
+        tail_bound=10.,
+        num_blocks=10,
+        use_batch_norm=True
+    )
+    return Flow(transform, base_dist)
+
+
+def get_dataloaders(file='end_points.pt', 
+                    batch_size: int = 64, 
+                    val_ratio: float = 0.1, 
+                    save_data: bool = True, 
+                    simulator_args: Tuple[int,...] = None) -> Tuple[DataLoader, DataLoader]:
+    """Utility function for obtaining training and validation dataloaders
+
+    Args:
+        file (str, optional): File to load data from, if available. Defaults to 'end_points.pt'.
+        batch_size (int, optional): Defaults to 64.
+        val_ratio (float, optional): Fraction of the data to use for validation.  Defaults to 0.1.
+        save_data (bool, optional): If True then new data generated will be saved to disk. Defaults to True.
+        simulator_args (Tuple[int,...], optional): _description_. Defaults to None.
+
+    Returns:
+        _type_: _description_
+    """
+    
+    cut_off = round(len(data)*val_ratio)
+
+
+    if exists(file):
+        data = torch.load(file)
+    else:
+        if simulator_args is None:
+            simulator = Simulator()
+        else:
+            simulator = Simulator(*simulator_args)
+
+        print('No data found, simulating 5000 new data points.')
+        data = torch.stack([simulator()[0] for _ in tqdm(range(5000))])
+
+        if save_data:
+            torch.save(data, file)
+
+    train_data = data[cut_off:]
+    val_data = data[:cut_off]
+    train_loader = DataLoader(train_data, batch_size, shuffle=True)
+    val_loader = DataLoader(val_data, batch_size, shuffle=True)
+
+    return train_loader, val_loader
+
+
+class Trainer:
+    """Class for training a normalising flow.
+
+    Attributes:
+        density_estimator - a trainable estimator with 
+    
+
+    """
+    def __init__(self,
+                 train_loader: DataLoader,
+                 val_loader: DataLoader,
+                 density_estimator: Module,
+                 early_stop_bound: int = 20,
+                 max_epochs: int = 500,
+                 optimizer: Optimizer = Adam,
+                 base_learning_rate: float = 5e-3,
+                 use_lr_scheduler: bool = True,
+                 ) -> None:
+        """
+        Inits Trainer.
+
+        Args:
+            train_loader (DataLoader): dataloader class for training data.
+            val_loader (DataLoader): dataloader class for validation data.
+            density_estimator (Module): normalising flow or equivalent.
+            early_stop_bound (int, optional): Used in early stopping condition - the number of 
+            rounds of no improvement after which to stop. Defaults to 20.
+            max_epochs (int, optional): For if early stopping does not occur. Defaults to 500.
+            optimizer (Optimizer, optional): Defaults to Adam.
+            base_learning_rate (float, optional): Defaults to 5e-3.
+            use_lr_scheduler (bool, optional): If True, uses plateau rate scheduler. Defaults to True.
+        """
+        
+        self.density_estimator = density_estimator
+        self._train_loader = train_loader
+        self._val_loader = val_loader
+        self._early_stop_bound = early_stop_bound
+        self._max_epochs = max_epochs
+        self._trained_epochs = 0
+
+        optimisation_parameters = density_estimator.parameters()
+        self._optimizer = optimizer(optimisation_parameters, lr=base_learning_rate)
+        if use_lr_scheduler:
+            self._lr_scheduler=ReduceLROnPlateau(self._optimizer, patience=25)
+
+        self.train_losses = []
+        self.val_losses = []
+
+
+    def _loss(self, params: Tensor):
+        """Cross-entropy loss (equivalent to KL divergence)
+
+        Args:
+            params (Tensor): paramters sampled from the underlying distribution.
+
+        Returns:
+            Tensor: value of loss.
+        """
+        return -torch.mean(self.density_estimator.log_prob(params))
+
+    def training_loop(self, dataloader: DataLoader):
+        """Executes a training epoch.
+
+        Args:
+            dataloader (DataLoader): Expects a dataloader with unlabelled data. 
+
+        Returns:
+            Tensor: mean loss
+        """
+        self.density_estimator.train()
+        total_loss = 0.
+        for params in enumerate(dataloader):
+            loss = self._loss(params)
+            total_loss+=loss.detach()
+            self._optimizer.zero_grad()
+            loss.backward()
+            self._optimizer.step()
+        mean_loss = total_loss/len(dataloader.dataset) 
+        return mean_loss
+    
+    def validation_loop(self, dataloader: DataLoader):
+        """Checks validation loss
+
+        Args:
+            dataloader (DataLoader): Expects a dataloader with unlabelled data. 
+        """
+        self.density_estimator.eval()
+        loss = self._loss(dataloader.dataset)
+        return loss
+    
+    def log_and_print(self, train_loss, val_loss, since_improvement):
+        self.train_losses.append(train_loss)
+        self.val_losses.append(val_loss)
+        print(f'epoch = {self._trained_epochs}, train loss = f{train_loss}, val loss = f{val_loss}\
+              epochs since improvement = f{since_improvement} \r')
+
+
+    def train(self, epochs = 200):
+        """Trains the density estimator. If the total epochs trained 
+        already exceeds max_epochs raises an exception.
+
+        Args:
+            epochs (int, optional): Epochs to train for. Defaults to 200.
+
+        Returns:
+            Module: Trained normalising flow
+        """
+        if self._trained_epochs >=self._max_epochs:
+            raise Exception(f"Already trained density estimator for the \
+                            maximum number of epochs ({self._max_epochs})")
+
+        best_loss = torch.inf
+        rounds_since_improvement = 0
+        for i in range(epochs):
+            self._trained_epochs += 1
+            train_loss = self.training_loop()
+            val_loss = self.validation_loop
+            if val_loss < best_loss:
+                best_loss = val_loss
+                rounds_since_improvement = 0
+            else:
+                rounds_since_improvement+=1
+
+            self.log_and_print(train_loss, val_loss, rounds_since_improvement)
+            if rounds_since_improvement == self._early_stop_bound:
+                break
+
+        # Return for the sake of convenience. Also accessible as attribute of Trainer object.
+        return self.density_estimator
