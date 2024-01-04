7c8139391db2ac984b53fc3de52fc8e902220769
sragy <sammyragy@gmail.com>
2024-01-04 23:31:20 +0000

    Corrected some code for grid tracker

--- tracking.py
+++ tracking.py
@@ -3,14 +3,21 @@ from torch import Tensor
 from nflows.flows import Flow
 import warnings
 
+
+
+
 class Tracker:
-    """Tracker class. Given a trained density estimator, searches for object.
+    """Tracker class. Given a trained density estimator, searches for object. Assumes scans in small
+    square regions with size determined by scan_radius. The area_of_interest refers to the full search
+    region, not just the immediate scan area. If initialised with a set of points to check, then will
+    scan around these points. Otherwise, it is recommended to initialise as a sampling tracker or
+    grid tracker. The grid tracker provides full coverage of the area of interest.
 
     Attributes:
         density_estimator: A normalising flow.
         scan_radius: Half the length of the square in which to scan.
-        area_of_interest (Tensor): Area in which to search - points outside here are ignored.
-        points_to_check (Tensor): Points around which to check
+        area_of_interest (Tensor, optional): Area in which to search - points outside here are ignored.
+        points_to_check (Tensor, optional): Points around which to check
     Methods:
         init_sampling_tracker: generates points from sampling density estimator, then inits with these
         init_grid_tracker: generates points on a grid within the area of interest, then inits with these
@@ -68,6 +75,7 @@ class Tracker:
             area_of_interest (Tensor, optional): _description_. Defaults to None.
             points_to_check (Tensor, optional): _description_. Defaults to None.
         """
+        
 
     def _rank_points(self, points: Tensor):
         """Ranks points by likelihood. Most likely first.
@@ -109,7 +117,7 @@ class Tracker:
             warnings.warn("not all points to check are in area of interest, some will be ignored")
        
         return points[mask]
-    
+
     def _generate_scan_area(self, point: Tensor):
         """Generates an area of self.scan_radius centred on the given point 
 
@@ -123,6 +131,62 @@ class Tracker:
         right_and_top = point + self.scan_radius
 
         return torch.stack([left_and_bottom, right_and_top], dim=1)
+    
+
+    def _generate_AOI_from_points(self, points: Tensor, mult_factor: float = 1.3):
+        """Generates area of interest as function of the minimum enclosing box of the points.
+        The multiplication factor grows or shrinks the minimum enclosing box.
+
+        Args:
+            points (Tensor): The points to be enclosed by the bounding box
+            mult_factor (float, optional): Multiplies size of bounding box. Defaults to 1.
+        Returns:
+            Tensor: _description_
+        """
+        axes_max = points.max(dim=0)
+        axes_min = points.min(dim=0)
+
+
+        mid_point = (axes_max + axes_min)/2
+        dist_from_mid = mult_factor* (axes_max - axes_min)/2
+
+        right_and_top = mid_point+dist_from_mid
+        left_and_bottom = mid_point - dist_from_mid
+
+        return torch.stack([left_and_bottom, right_and_top], dim=1)
+    
+    def _generate_points_from_grid_centres(self, area_of_interest: Tensor = None):
+        """Generates a grid, defined by the points at the centre of the grid squares.
+        The grid squares are 
+
+        Args:
+            area_of_interest (Tensor, optional): Area in which to draw the grid. Defaults to None.
+        """
+        
+        if area_of_interest is None:
+            area_of_interest = self.area_of_interest
+
+        left, right = area_of_interest[0]
+        top, bottom = area_of_interest[1]
+
+        # The minimum number of steps to cross the whole space
+        horiz_steps = torch.ceil((right-left)/(2*self.scan_radius))
+        vert_steps= torch.ceil((top-bottom)/(2*self.scan_radius))
+
+        # Adjusted step lengths, to ensure we can fit the final step in each direction.
+        # Will be <= scan_radius (there are probably better solutions). 
+        h_step_length = (right-left)/horiz_steps
+        v_step_length = (right-left)/vert_steps
+
+        horiz_start = left + h_step_length #centre of leftmost column
+        vert_start = bottom + v_step_length #centre of bottom row
+
+        horizontal_centres = torch.arange(horiz_start, right, h_step_length)
+        vertical_centres = torch.arange(vert_start, top, v_step_length)
+
+        self.h_step_length = h_step_length
+        self.v_step_length = v_step_length
+        self.grid = torch.cartesian_prod(horizontal_centres, vertical_centres)
 
 
     def simulate_tracking(self, true_point: Tensor):
@@ -171,17 +235,9 @@ class SamplingTracker(Tracker):
             density_estimator (Flow): A density estimator for trajectory end points
             scan_radius (int): 'radius' of scanning area; half side-length of square scanning region.
             area_of_interest (Tensor, optional): area_of_interest. Defaults to None.
-            points_to_check (Tensor, optional): _description_. Defaults to None.
+            num_samples (int): _description_. Defaults to 1000.
         """
         super().init_sampling_tracker(density_estimator, scan_radius, area_of_interest, num_samples)
-
-    def _generate_points_to_check(self, num_samples: int):
-        self.points_to_check = self.density_estimator.sample(num_samples)    
-    
-    def simulate_tracking(self, true_point: Tensor):
-        if self.points_to_check is None:
-           self._generate_points_to_check(1000)
-        return super().simulate_tracking(true_point)
 
 class GridTracker(Tracker):
     def __init__(self, density_estimator: Flow,
